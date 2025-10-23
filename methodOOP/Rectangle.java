cdf4516950e0c536d7f39ea605021ba9c9c761b8
DangQuangTien <136679780+tienml@users.noreply.github.com>
2025-10-23 22:03:39 +0700

    Add Rectangle class with properties and methods
    Create class Rectangle to represent a rectangle object.
    
    Includes:
    - Attributes: width (double), height (double)
    - Default constructor and parameterized constructor
    - Getter and setter methods
    - toString() method to display rectangle information
    - getArea() to calculate the area of the rectangle
    - getPerimeter() to calculate the perimeter of the rectangle
    - isSquare() to check if the rectangle is a square

---
+++ methodOOP/Rectangle.java
@@ -0,0 +1,41 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle() {
+        this.width = 0.0;
+        this.height = 0.0;
+    }
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getArea() {
+        return width * height;
+    }
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+    public boolean isSquare() {
+        return width == height;
+    }
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
